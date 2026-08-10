dffe688f34de1108cb87792962212b5f49bedf03
brutumfulmen97 <109813663+brutumfulmen97@users.noreply.github.com>
2026-08-10 13:59:10 +0200

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s: string): boolean {
+        const stack = [];
+        const map = {
+            ")": "(",
+            "]": "[",
+            "}": "{",
+        };
+
+        for (let c of s) {
+            if (map[c]) {
+                if (stack.length > 0 && stack[stack.length - 1] === map[c]) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.length === 0;
+    }
+}
